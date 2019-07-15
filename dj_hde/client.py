5027f4222f3bc5b468c2f81bcbfbcf11ef89f991
devxplorer <devxplorer@gmail.com>
2019-07-15 13:06:41 +0300

    api_credentials -> self.config

--- dj_hde/client.py
+++ dj_hde/client.py
@@ -7,14 +7,14 @@ from .exceptions import NoActiveCredentials, MultipleActiveCredentials
 class DjHelpDeskEddyClient(HelpDeskEddyClient):
     def __init__(self):
         try:
-            api_credentials = Config.objects.get(is_active=True)
+            self.config = Config.objects.get(is_active=True)
         except Config.DoesNotExist:
             raise NoActiveCredentials
         except Config.MultipleObjectsReturned:
             raise MultipleActiveCredentials
 
         super(DjHelpDeskEddyClient, self).__init__(
-            domain=api_credentials.domain,
-            email=api_credentials.email,
-            api_key=api_credentials.api_key,
+            domain=self.config.domain,
+            email=self.config.email,
+            api_key=self.config.api_key,
         )
