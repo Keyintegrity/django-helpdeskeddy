08471ff616d818b8051d6bb4c0d3c66ba10ef8b2
Artyom Opanchuk <artyom.opanchuk@gmail.com>
2020-03-30 18:38:06 +0300

    Add supported Python versions

--- setup.py
+++ setup.py
@@ -24,6 +24,12 @@ setup(
         'Framework :: Django',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
