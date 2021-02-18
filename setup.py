4605b86805408e6868f7acb576297d847bf1b4a4
borodulin <borodulin_da@srvhub.ru>
2021-02-18 10:21:07 +0300

    Add support for mysqlclient 2.0.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         "helpdeskeddy-api-client @ https://github.com/Keyintegrity/helpdeskeddy-api-client/archive/v0.0.2.zip#egg=helpdeskeddy-api-client-0.0.2",
         "Django>=1.11.22,<3.0",
-        "mysqlclient>=1.3.13,<2.0",
+        "mysqlclient>=1.3.13,<2.0.4",
         "django-mysql>=3.2.0,<4.0",
     ],
     classifiers=[
